0c2c2b017fd0acee9b72af0deb6534bca8af736d
Coen <boelhouwers.coen@gmail.com>
2017-08-14 09:16:22 +0200

    More fun testing.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
+import javafx.animation.*;
 import javafx.application.Application;
 import javafx.application.ConditionalFeature;
 import javafx.application.Platform;
 import javafx.geometry.Point3D;
 import javafx.scene.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -28,54 +26,78 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		Rectangle rect0 = new Rectangle(-25, -25, 50, 50);
+		Rectangle rect0 = new Rectangle(500, 500);
 		rect0.setFill(new Color(1.0, 0.0, 0.0, 0.3));
-		rect0.getTransforms().add(new Rotate(-90, new Point3D(0, 1, 0)));
 
-		Rectangle rect1 = new Rectangle(-20, -20, 40, 40);
-		rect1.setFill(new Color(0.0, 0.0, 1.0, 0.3));
+		Rectangle rect1 = new Rectangle(500, 500);
+		rect1.getTransforms().add(new Rotate(-90, 500, 0, 0, new Point3D(0, 1, 0)));
+		rect1.setFill(Color.GREEN);
 
-		Rectangle rect2 = new Rectangle(-25, -25, 50, 50);
-		rect2.setFill(Color.GREEN);
+		Rectangle rect2 = new Rectangle(500, 500);
+		rect2.setFill(Color.BLUE);
 		rect2.getTransforms().add(new Rotate(90, new Point3D(0, 1, 0)));
 
-		Rectangle rect3 = new Rectangle(-25, -25, 50, 50);
+		Rectangle rect3 = new Rectangle(500, 500);
 		rect3.setFill(Color.ORANGE);
-		rect3.getTransforms().add(new Rotate(-90, 50, 0, 0, new Point3D(0, 1, 0)));
+		rect3.getTransforms().add(new Rotate(180, 250, 0, -250, new Point3D(0, 1, 0)));
 
-		Rectangle rect4 = new Rectangle(25, 25, 50, 50);
-		rect4.setFill(Color.CYAN);
-		rect4.getTransforms().add(new Rotate(90, 50, 0, 0, new Point3D(0, 1, 0)));
+		Group cube = new Group(rect3, rect1, rect2, rect0);
+		cube.setTranslateX(-250);
+		cube.setTranslateY(-500);
+		cube.setTranslateZ(250);
 
-		Circle circle = new Circle(0, 50, 50, Color.WHITE);
-		circle.setTranslateZ(-50);
+		Circle circle = new Circle(0, 0, 500, Color.WHITE);
 		circle.getTransforms().add(new Rotate(90, new Point3D(1, 0, 0)));
 
-		PerspectiveCamera camera = new PerspectiveCamera(true);
-		camera.setNearClip(200);
-		camera.setFarClip(500);
+		ImageView imageView = new ImageView("/snake_background2.png");
+		imageView.getTransforms().add(new Rotate(90, new Point3D(1, 0, 0)));
+		imageView.setPreserveRatio(true);
+		imageView.setFitHeight(6000);
+		imageView.setTranslateX(-2000);
+		imageView.setTranslateY(100);
+		imageView.setTranslateZ(-2000);
 
-		Group group = new Group(rect2, rect3, rect4, circle, rect0, rect1);
-		Rotate rotate = new Rotate(0,  0, 0, 0, new Point3D(0, 1, 0));
+		PerspectiveCamera camera = new PerspectiveCamera(true);
+		camera.setNearClip(500);
+		camera.setFarClip(8000);
+
+		Group group = new Group(imageView, circle, cube);
+
+		Rotate yRotation = new Rotate(0,  0, 0, 0, new Point3D(0, 1, 0));
+		Rotate xRotation = new Rotate(0,  0, 0, 0, new Point3D(1, 0, 0));
+		Translate distance = new Translate(0, 0, -350);
 		camera.getTransforms().addAll(
-				//new Translate(50, 50, 50),//pivot
-				rotate,
-				new Rotate(-20, new Point3D(1, 0, 0)),
-				new Translate(0, 0, -350));
+				new Translate(0, -250, 0),//pivot
+				yRotation,
+				xRotation,
+				distance);
 
 		Timeline timeline = new Timeline(
 				new KeyFrame(Duration.ZERO,
-						new KeyValue(rotate.angleProperty(), 0)),
+						new KeyValue(yRotation.angleProperty(), 0)),
 				new KeyFrame(Duration.seconds(15),
-						new KeyValue(rotate.angleProperty(), 360))
+						new KeyValue(yRotation.angleProperty(), 360))
 		);
+		timeline.setDelay(Duration.seconds(2));
 		timeline.setCycleCount(Animation.INDEFINITE);
 		timeline.playFromStart();
+
+		Timeline timeline2 = new Timeline(
+				new KeyFrame(Duration.ZERO,
+						new KeyValue(xRotation.angleProperty(), -10),
+						new KeyValue(distance.zProperty(), -2000)),
+				new KeyFrame(Duration.seconds(5),
+						new KeyValue(xRotation.angleProperty(), -40, Interpolator.EASE_BOTH),
+						new KeyValue(distance.zProperty(), -5000, Interpolator.EASE_BOTH))
+		);
+		timeline2.setAutoReverse(true);
+		timeline2.setCycleCount(Animation.INDEFINITE);
+		timeline2.playFromStart();
 
 		System.out.println(Platform.isSupported(ConditionalFeature.SCENE3D));
 
 		Scene scene = new Scene(group, 600, 400, true, SceneAntialiasing.BALANCED);
-		scene.setFill(Color.GRAY);
+		scene.setFill(Color.BLACK);
 		scene.setCamera(camera);
 
 		primaryStage.setScene(scene);
