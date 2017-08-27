6ac509ffc23c3f9d90f50dfa17732f84ef125169
CoenB95 <boelhouwers.coen@gmail.com>
2017-08-27 18:06:21 +0200

    More sides.

--- src/RectBlock.java
+++ src/RectBlock.java
@@ -32,36 +32,39 @@ public class RectBlock extends Group {
 	}
 
 	public void setRaised() {
-		getChildren().addAll(createSide(Side.FRONT), createSide(Side.BACK));
+		getChildren().addAll(createSide(Side.FRONT), createSide(Side.BACK),
+				createSide(Side.LEFT), createSide(Side.RIGHT));
 		setTranslateY(-height);
 	}
 
 	private ImageView createSide(Side side) {
 		switch (side) {
 			case TOP:
-				return createSide(0.25, 0, -1,
-						new Translate(-width/2, -height/2, depth/2),
-						new Rotate(-90, new Point3D(1, 0, 0)));
+				return createSide(0.25, 0, 270, -1);
 			case FRONT:
-				return createSide(0.25, 0.25, 0,
-						new Translate(-width/2, -height/2, -depth/2));
+				return createSide(0.25, 0.25, -1, 0);
 			case BACK:
-				return createSide(0.75, 0.25, 180,
-						new Rotate(-180, new Point3D(1, 0, 0)),
-						new Translate(-width/2, -height/2, -depth/2));
+				return createSide(0.75, 0.25, -1, 180);
+			case LEFT:
+				return createSide(0, 0.25, -1, 90);
+			case RIGHT:
+				return createSide(0.5, 0.25, -1, 270);
 		}
 		return null;
 	}
 
-	private ImageView createSide(double percentX, double percentY, double horizontalAngle, Transform... transforms) {
+	private ImageView createSide(double percentX, double percentY, double xAngle, double yAngle) {
 		ImageView side = new ImageView(image);
 		side.setFitWidth(width);
 		side.setFitHeight(height);
 		side.setViewport(new Rectangle2D(image.getWidth() * percentX, image.getHeight() * percentY,
 				image.getWidth() * 0.25, image.getHeight() * 0.25));
-		side.getTransforms().addAll(transforms);
-		if (horizontalAngle >= 0)
-			side.visibleProperty().bind(AngleBindings.isBetween(BoxMain.cameraAngle, horizontalAngle - 100, horizontalAngle + 100));
+		if (xAngle >= 0) side.getTransforms().add(new Rotate(xAngle, new Point3D(1, 0, 0)));
+		if (yAngle >= 0) side.getTransforms().add(new Rotate(yAngle, new Point3D(0, 1, 0)));
+		side.getTransforms().add(new Translate(-width/2, -height/2, -depth/2));
+
+		if (yAngle >= 0)
+			side.visibleProperty().bind(AngleBindings.isBetween(BoxMain.cameraAngle, yAngle - 110, yAngle + 110));
 		return side;
 
 	}
